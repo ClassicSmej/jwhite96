df94490bafdface5b5fc6b9dc22a81cfebc74666
ClassicSmej <jamesw.white96@gmail.com>
2021-11-19 22:08:27 +0000

    TypewriterJS

--- Website/Application/static/js/typer.js
+++ Website/Application/static/js/typer.js
@@ -1,22 +1,25 @@
 
+
 var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
-    loop: true
+    loop: true,
+    delay: 100,
 });
 
-typewriter.typeString("<label style='color: #fc1d33'>Hi I'm James White</label>")
+typewriter.typeString("<label id='writer1'>Hi I'm James White</label>")
     .pauseFor(2500)
     .deleteAll()
-    .typeString("<label style='color: #d7e61a'>I'm a Cloud Specialist</label>")
+    .typeString("<label id='writer2'>AWS Cloud Specialist</label>")
     .pauseFor(2500)
     .deleteAll()
-    .typeString("<label id='cloud' style='color: #99C6E5'>I Build Cloud Services</label>")
+    .typeString("<label id='writer3'>I Build Cloud Services</label>")
     .pauseFor(2500)
     .deleteAll()
-    .typeString("<label style='color: #9CE6AF'> I Build Websites</label>")
+    .typeString("<label id='writer4'>I Build Websites</label>")
     .pauseFor(2500)
     .deleteAll()
-    .typeString("<label style='color: #B299E6'>Go Explore</label>")
+    .typeString("<label id='writer5'>Go Explore</label>")
     .pauseFor(2500)
     .start();
+
