692a066cc13edc589ff0883876dece8425b22bef
James White <jamesw.white96@gmail.com>
2021-11-26 16:46:13 +0000

    Dot Nav - Working

--- Website/Application/static/js/dotnav.js
+++ Website/Application/static/js/dotnav.js
@@ -2,33 +2,39 @@
 function activateNavigation() {
     const sections = document.querySelectorAll(".section");
     const navContainer = document.createElement("nav");
-    const navItems = Array.from(sections).map(section => {
-        return `
-            <div class="nav-item" data-for-section"${section.id}">
-                <a href="#${section.id}" class="nav-link"></a>
-                <span class="nav-label">${section.dataset.label}</span>
-            </div>
-        `;
+    const navItems = Array.from(sections).map((section) => {
+      return `
+                      <div class="nav-item" data-for-section="${section.id}">
+                          <a href="#${section.id}" class="nav-link"></a>
+                          <span class="nav-label">${section.dataset.label}</span>
+                      </div>
+                  `;
     });
-
+  
     navContainer.classList.add("nav");
     navContainer.innerHTML = navItems.join("");
-
-    const observer = new IntersectionObserver(entries => {
-        document.querySelectorAll(".nav-link").forEach(navLink => {
-            navLink.classList.remove("nav-link-selected")
+  
+    const observer = new IntersectionObserver(
+      (entries) => {
+        document.querySelectorAll(".nav-link").forEach((navLink) => {
+          navLink.classList.remove("nav-link-selected");
         });
-
-        const visableSection = entries.filter(entry => entry.isIntersecting)[0];
-
-        document.querySelector(`.nav-item[data-for-section="${visableSection.target.id}"] .nav-link`).classList.add("nav-link-selected");
-
-    }, { threshold: 0.5 } );
-
-    sections.forEach(section => observer.observe(section));
-
+  
+        const visibleSection = entries.filter((entry) => entry.isIntersecting)[0];
+  
+        document
+          .querySelector(
+            `.nav-item[data-for-section="${visibleSection.target.id}"] .nav-link`
+          )
+          .classList.add("nav-link-selected");
+      },
+      { threshold: 0.5 }
+    );
+  
+    sections.forEach((section) => observer.observe(section));
+  
     document.body.appendChild(navContainer);
-    
-}
-
-activateNavigation()
+  }
+  
+  activateNavigation();
+  
