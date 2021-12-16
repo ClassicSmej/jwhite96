5601a816cc88114bb68397329568abe08dfe30e2
James White <jamesw.white96@gmail.com>
2021-12-16 17:07:28 +0000

    Basic Terraform Configuration

---
+++ Terraform/main.tf
@@ -0,0 +1,97 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = var.regions[0]
+    access_key = var.keys["access"]
+    secret_key = var.keys["secret"]
+}
+
+resource "aws_vpc" "vpc" {
+    cidr_block = var.vpc_cidr_block
+
+    tags = {
+      "Name" = "TF-VPC"
+    }
+}
+
+resource "aws_subnet" "subnet1" {
+    vpc_id = aws_vpc.vpc.id
+    cidr_block = var.subnet_cidr[0]
+    availability_zone = var.azs[0]
+
+    tags = {
+        "Name" = "TF-Subnet-1"
+    }
+}
+
+resource "aws_subnet" "subnet2" {
+    vpc_id = aws_vpc.vpc.id
+    cidr_block = var.subnet_cidr[1]
+    availability_zone = var.azs[1]
+
+    tags = {
+        "Name" = "TF-Subnet-2"
+    }
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.vpc.id
+
+    tags = {
+        "Name" = "TF-IGW"
+    }
+}
+
+resource "aws_route_table" "routetable" {
+    vpc_id = aws_vpc.vpc.id
+
+    route = []
+
+    tags = {
+        "Name" = "TF-RT"
+    }
+}
+
+resource "aws_route" "route" {
+    route_table_id = aws_route_table.routetable.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+    depends_on = [ aws_route_table.routetable ]
+}
+
+resource "aws_route_table_association" "rta1" {
+    subnet_id = aws_subnet.subnet1.id
+    route_table_id = aws_route_table.routetable.id
+}
+
+resource "aws_route_table_association" "rta2" {
+    subnet_id = aws_subnet.subnet2.id
+    route_table_id = aws_route_table.routetable.id
+}
+
+resource "aws_instance" "instance1" {
+    ami = var.ami["Linux4"]
+    instance_type = var.instance_type
+    subnet_id = aws_subnet.subnet1.id
+
+    tags = {
+        "Name" = "TF-Instance-1"
+    }
+}
+
+resource "aws_instance" "instance2" {
+    ami = var.ami["Linux5"]
+    instance_type = var.instance_type
+    subnet_id = aws_subnet.subnet2.id
+
+    tags = {
+        "Name" = "TF-Instance-2"
+    }
+}
---
+++ Terraform/variables.tf
@@ -0,0 +1,43 @@
+variable "keys" {
+    type = map
+    default = {
+        "access" = "AKIATDEK35FN5622Z45Y"
+        "secret" = "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"
+    }
+}
+
+variable regions {
+    type = list(string)
+    default = [
+        "us-east-1",
+        "us-west-1"
+    ]
+}
+
+variable vpc_cidr_block {
+    type = string
+    default = "10.0.0.0/16"
+}
+
+variable subnet_cidr {
+    type = list(string)
+    default = [ "10.0.10.0/24", "10.0.20.0/24" ]
+}
+
+variable azs {
+    type = list(string)
+    default = [ "us-east-1a", "us-east-1b" ]
+}
+
+variable ami {
+    type = map
+    default = {
+        "Linux5" = "ami-0ed9277fb7eb570c9"
+        "Linux4" = "ami-061ac2e015473fbe2"
+    }
+}
+
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+}
